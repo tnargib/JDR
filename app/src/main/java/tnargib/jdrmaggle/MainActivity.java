744ca0f9b4d8e687dd8e164f8a7fbbd4995205a3
TNARGIB <sbrigant@live.fr>
2015-10-03 19:22:37 +0200

    first commit

---
+++ app/src/main/java/tnargib/jdrmaggle/MainActivity.java
@@ -0,0 +1,41 @@
+package tnargib.jdrmaggle;
+
+import java.util.List;
+import java.util.Vector;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
+public class MainActivity extends FragmentActivity {
+
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.viewpager);
+
+        // Création de la liste de Fragments que fera défiler le PagerAdapter
+        List fragments = new Vector();
+
+        // Ajout des Fragments dans la liste
+        fragments.add(Fragment.instantiate(this,CthulhuFragment.class.getName()));
+        fragments.add(Fragment.instantiate(this, CyberFragment.class.getName()));
+        fragments.add(Fragment.instantiate(this, NaheulFragment.class.getName()));
+
+
+
+        // Création de l'adapter qui s'occupera de l'affichage de la liste de
+        // Fragments
+        this.mPagerAdapter = new MyPagerAdapter(super.getSupportFragmentManager(), fragments);
+
+        ViewPager pager = (ViewPager) super.findViewById(R.id.viewpager);
+        // Affectation de l'adapter au ViewPager
+        pager.setAdapter(this.mPagerAdapter);
+
+
+    }
+}
+
